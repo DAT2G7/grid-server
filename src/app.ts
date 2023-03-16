0a14cadf78e54488c6e0b0d7792441216334f849
bliztle <asbjoern.r.e@gmail.com>
2023-03-16 21:13:52 +0100

    Cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,32 @@
-import { apiClientRouter, apiProjectRouter } from './routes/api';
-import { clientRouter, indexRouter, projectRouter } from './routes';
+import { apiClientRouter, apiProjectRouter } from "./routes/api";
+import { clientRouter, indexRouter, projectRouter } from "./routes";
 
-import { config as dotenvConfig } from 'dotenv';
-import express from 'express';
+import { config as dotenvConfig } from "dotenv";
+import express from "express";
 
 dotenvConfig();
 
 // init app
 const app = express();
-// const port = 3000;
 const port = process.env.PORT || 3000;
 
 // Use pug for views
-app.set('view engine', 'pug');
-app.set('views', 'src/public/views');
+app.set("view engine", "pug");
+app.set("views", "src/public/views");
 
 // Set endpoint for static assets
-app.use('/static', express.static('dist/public/static'));
+app.use("/static", express.static("dist/public/static"));
 
 // Register view routes
-app.use('/client', clientRouter);
-app.use('/project', projectRouter);
-app.use('/', indexRouter);
+app.use("/client", clientRouter);
+app.use("/project", projectRouter);
+app.use("/", indexRouter);
 
 // Register API routes
-app.use('/api/client', apiClientRouter);
-app.use('/api/project', apiProjectRouter);
+app.use("/api/client", apiClientRouter);
+app.use("/api/project", apiProjectRouter);
 
 // Start server
 app.listen(port, () => {
-    console.log('Server started on port', port);
+    console.log("Server started on port", port);
 });
