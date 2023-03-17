b9ea340f3472b65b01961adc6dcd7068bbd8835d
bliztle <asbjoern.r.e@gmail.com>
2023-03-17 09:34:17 +0100

    Now render the correct page

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -4,5 +4,5 @@ import { RequestHandler } from "express";
  * Serve the project owner index page
  */
 export const renderIndex: RequestHandler = (_req, res) => {
-    res.render("client/index");
+    res.render("project/index");
 };
