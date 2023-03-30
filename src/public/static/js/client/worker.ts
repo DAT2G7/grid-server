bd786c73890b0b0bf57b82573df45098111d03d2
Markus Dam <markus.dam123@gmail.com>
2023-03-30 16:33:40 +0200

    add a type for the response the client gets from `/api/client/setup`

--- src/public/static/js/client/worker.ts
+++ src/public/static/js/client/worker.ts
@@ -1,4 +1,4 @@
-import { UUID } from "../../../../types/brand.types";
+import { ClientTask } from "../../../../types/body.types";
 
 // extend the window object so that we can put our own methods on `self`
 // this isn't techinally 100% correct, since the type of `self` in a webworker is `DedicatedWorkerGlobalScope`,
@@ -26,12 +26,7 @@ const run = async () => {
     // TODO: it looks like the /api/client/setup endpoint doens't return any core id, so we don't know how to get that
     // also, we don't get a task id, so we can't fetch our data
     // for now we'll just assume that we get that data back, and this is gonna be broken until it's fixed
-    const setupData: {
-        projectId: UUID;
-        jobId: UUID;
-        coreId: UUID;
-        taskId: UUID;
-    } = await response.json();
+    const setupData: ClientTask = await response.json();
 
     const getData: () => Promise<unknown | never> = async () => {
         const response = await fetch(
---
+++ src/types/body.types.ts
@@ -0,0 +1,9 @@
+import { UUID, CoreUUID, JobUUID, TaskUUID } from "./brand.types";
+
+export interface ClientTask {
+    // TODO: use the `ProjectUUID` type from another branch once that gets merged
+    projectId: UUID,
+    coreId: CoreUUID,
+    jobId: JobUUID,
+    taskId: TaskUUID,
+}
