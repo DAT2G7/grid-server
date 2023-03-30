ba012822746cfd7db9a1c574d59de47f6e832251
Markus Dam <markus.dam123@gmail.com>
2023-03-30 16:41:50 +0200

    run prettier on body.types.ts

--- src/types/body.types.ts
+++ src/types/body.types.ts
@@ -2,8 +2,8 @@ import { UUID, CoreUUID, JobUUID, TaskUUID } from "./brand.types";
 
 export interface ClientTask {
     // TODO: use the `ProjectUUID` type from another branch once that gets merged
-    projectId: UUID,
-    coreId: CoreUUID,
-    jobId: JobUUID,
-    taskId: TaskUUID,
+    projectId: UUID;
+    coreId: CoreUUID;
+    jobId: JobUUID;
+    taskId: TaskUUID;
 }
