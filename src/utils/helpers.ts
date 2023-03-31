f959dca14d64717a32772eadb28e5eab7b409777
HPT-I <isak@hauptmann.tech>
2023-03-31 12:02:10 +0200

    simplified isDefined

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,4 @@
-export function isDefined<T>(value: T | undefined): value is T {
-    return value !== undefined && value !== null;
+// A non strict check for null also checks for undefined.
+export function isDefined<T>(value: T | undefined | null): value is T {
+    return value != null;
 }
