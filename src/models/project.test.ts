94c19bc3df5fadf9000d905326fb108737698224
BuildTools <marcusanholt@gmail.com>
2023-04-01 19:44:53 +0200

    Move project model test data to separate file

--- src/models/project.test.ts
+++ src/models/project.test.ts
@@ -1,6 +1,6 @@
-import { UUID } from "../types/brand.types";
-import { Job, Project } from "../types/global.types";
 import { ProjectModel } from "./project.model";
+import { testData, testJob, testProject } from "./project.test.data";
+import { Job, Project } from "../types/global.types";
 import JsonDB from "../services/json.db";
 import "jest-extended";
 
@@ -9,7 +9,7 @@ describe("ProjectModel", () => {
 
     test("can initialize the test model", () => {
         testDB = new ProjectModelMock("test.json");
-        testDB.save = jest.fn().mockImplementation(() => {});
+        testDB.save = jest.fn();
         testDB.refresh();
 
         expect(testDB.data).toEqual(testData);
@@ -57,6 +57,7 @@ describe("ProjectModel", () => {
             expect(testDB.data.length).toEqual(length - 1);
         });
     });
+
     describe("getRandomJob", () => {
         it("can find random jobs", () => {
             //for specific project
@@ -151,7 +152,6 @@ describe("ProjectModel", () => {
             expect(testDB.data[0].jobs.length).toEqual(jobAmount - 1);
         });
     });
-    describe("removeProject", () => {});
 });
 
 class ProjectModelMock extends ProjectModel {
@@ -164,45 +164,3 @@ class ProjectModelMock extends ProjectModel {
         return this;
     }
 }
-
-const testData: Project[] = [
-    {
-        projectId: "ba5868ea-8e4d-4f50-87ee-c6bd01ad635e" as UUID,
-        jobs: [
-            {
-                jobId: "5eb9971f-e713-45b9-8584-8e2bc72a386b" as UUID,
-                coreId: "c945fe39-e77e-4b51-a7f4-229bba2ae648" as UUID,
-                projectId: "ba5868ea-8e4d-4f50-87ee-c6bd01ad635e" as UUID,
-                taskAmount: 200,
-                taskRequestEndpoint:
-                    "http://url/to/project/owner/taskRequstEndpoint",
-                taskResultEndpoint:
-                    "http://url/to/project/owner/taskResultEndpoint"
-            },
-            {
-                jobId: "495886eb-f6ee-4a65-bf0d-4f6f3aa2c599" as UUID,
-                coreId: "d7e36d1e-5f87-4b03-89ac-cb5767f4c61b" as UUID,
-                projectId: "ba5868ea-8e4d-4f50-87ee-c6bd01ad635e" as UUID,
-                taskAmount: 2,
-                taskRequestEndpoint:
-                    "http://url/to/project/owner/taskRequstEndpoint",
-                taskResultEndpoint:
-                    "http://url/to/project/owner/taskResultEndpoint"
-            }
-        ]
-    }
-];
-
-const testProject: Project = {
-    projectId: "d68993c6-050e-4ff5-8fb4-d7501386890c" as UUID,
-    jobs: []
-};
-
-const testJob: Job = {
-    jobId: "195886eb-f6ee-4a65-bf0d-4f6f3aa2c599" as UUID,
-    coreId: "d7e36d1e-5f87-4b03-89ac-cb5767f4c61b" as UUID,
-    projectId: "ba5868ea-8e4d-4f50-87ee-c6bd01ad635e" as UUID,
-    taskAmount: 100,
-    taskRequestEndpoint: "http://url/to/project/owner/taskRequstEndpoint",
-    taskResultEndpoint: "http://url/to/project/owner/taskResultEndpoint"
-};
