1bfc88e3e55fc5f622bd6e35bcf5e56b6b29ecce
bliztle <asbjoern.r.e@gmail.com>
2023-04-11 16:34:37 +0200

    Initial. Pushing to test on rpi

---
+++ src/public/service-worker/service-worker.ts
@@ -0,0 +1,84 @@
+/// <reference lib="webworker" />
+
+const _self = self as unknown as ServiceWorkerGlobalScope;
+
+/** Gets the default cache for network ressources */
+const getCache = () => caches.open("v1");
+
+/** Adds urls to ressources which should be pre-cached */
+const addResourcesToCache = async (resources: string[]) => {
+    const cache = await getCache();
+    await cache.addAll(resources);
+};
+
+/** Puts a ressource to the default cache */
+const putInCache = async (request: Request, response: Response) => {
+    const cache = await getCache();
+    await cache.put(request, response);
+};
+
+const createResponse = (message: string, status: number) =>
+    new Response(JSON.stringify({ error: { message } }), {
+        status,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+/** Attempts to get cached response. Otherwise fetches network ressource */
+const cacheFirst = async (request: Request): Promise<Response> => {
+    // Attempt cache
+    console.log("Attempting cache");
+    const fromCache = await caches.match(request);
+    if (fromCache) return fromCache;
+
+    // Check URL before attempting network. Dissalow requests to foreign domains
+    const url = new URL(request.url);
+    if (url.hostname !== self.location.hostname) {
+        console.log(
+            "Hostname not allowed.\n",
+            url.hostname,
+            self.location.hostname
+        );
+        return createResponse(
+            "Requesting custom ressources is not allowed, to protect the identities of our users",
+            403
+        );
+    }
+
+    // console.log("Returning not allowed");
+    // return createResponse(
+    //     "Requesting custom ressources is not allowed, to protect the identities of our users",
+    //     403
+    // );
+
+    // Attempt network
+    console.log("Attempting network");
+    try {
+        const responseFromNetwork = await fetch(request);
+        // Cache response. Cloned as the response can only be consumed once.
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch {
+        return createResponse("Network error", 408);
+    }
+};
+
+// Set up pages to be pre-cached
+_self.addEventListener("install", (event) => {
+    console.log("installing", event);
+    event.waitUntil(addResourcesToCache(["/", "/client", "/project"]));
+});
+
+// Intercept fetch requests to serve cached resources and block requests to foreign domains
+_self.addEventListener("fetch", async (event) => {
+    console.log("fetching", event.request.url);
+    event.respondWith(cacheFirst(event.request));
+});
+
+console.log("service worker loaded");
+
+// const exportMe = "exportMe";
+// export { exportMe };
+// export {};
+// module.exports = {};
--- src/public/static/js/client/client.ts
+++ src/public/static/js/client/client.ts
@@ -1,21 +1,60 @@
-console.log('Client running');
+console.log("Client running");
 
 if (window.Worker) {
-    console.log('Web worker supported');
+    console.log("Web worker supported");
 
     // Create web worker. This way is not ideal, but allows for a simpler build process.
-    const worker = new Worker('/static/js/client/worker.js');
+    const worker = new Worker("/static/js/client/worker.js");
 
     // The ideal would be to use this, but it requires a more complex build process.
     // const worker = new Worker(new URL("./worker.ts", import.meta.url));
 
     // Listen for messages from worker
-    worker.addEventListener('message', (event) => {
-        console.log('Message from worker:\n', event);
+    worker.addEventListener("message", (event) => {
+        console.log("Message from worker:\n", event);
 
         // Send message to worker
-        worker.postMessage({ type: 'message', data: 'Hello worker!' });
+        worker.postMessage({ type: "message", data: "Hello worker!" });
     });
 } else {
-    console.log('Web worker not supported');
+    console.log("Web worker not supported");
 }
+
+const registerServiceWorker = async () => {
+    if (navigator.serviceWorker) {
+        try {
+            // const registration = await navigator.serviceWorker.register(
+            //     "/static/js/service-worker.js",
+            //     {
+            //         scope: "/static/js/"
+            //     }
+            // );
+            const registration = await navigator.serviceWorker.register(
+                "/service-worker.js"
+                // {
+                //     scope: "/"
+                // }
+            );
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+            setTimeout(() => {
+                console.log("Attempting foreign fetch");
+                fetch("https://get.geojs.io/v1/ip/country.json?ip=8.8.8.8")
+                    .then((response) => {
+                        console.log("sw:", navigator.serviceWorker.controller);
+                        console.log("fetch status:", response.status);
+                    })
+                    .catch((e) => console.error("fetch error:", e));
+            }, 10000);
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+registerServiceWorker();
