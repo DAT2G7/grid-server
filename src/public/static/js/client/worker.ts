a8e1d35994f31eb79437d5e2b1ca572590fd543a
Markus Dam <markus.dam123@gmail.com>
2023-04-11 09:18:53 +0200

    remove erroneous comment

--- src/public/static/js/client/worker.ts
+++ src/public/static/js/client/worker.ts
@@ -20,10 +20,6 @@ const run = async () => {
         return;
     }
 
-    // TODO: this should be put in a type of its own
-    // TODO: it looks like the /api/client/setup endpoint doens't return any core id, so we don't know how to get that
-    // also, we don't get a task id, so we can't fetch our data
-    // for now we'll just assume that we get that data back, and this is gonna be broken until it's fixed
     const setupData: ClientTask = await response.json();
 
     const getData: () => Promise<unknown | never> = async () => {
