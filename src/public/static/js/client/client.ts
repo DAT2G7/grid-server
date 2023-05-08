b716633e660a87493a82a35df050f0c6b86f4831
MegaScooper <rz20dz@student.aau.dk>
2023-05-08 03:05:51 +0200

    Start Computing Button. Replace with toggle

--- src/public/static/js/client/client.ts
+++ src/public/static/js/client/client.ts
@@ -17,43 +17,44 @@ const run = () => {
         alert("Web worker not supported on device");
         return;
     }
-    const start = tryConfirm("Do you want to start computing?");
-    if (!start) {
-        return;
-    }
-    // Create web worker. This way is not ideal, but allows for a simpler build process.
-    worker = new Worker("/static/js/client/worker.js");
+    const computeButton = document.getElementById("computeButton");
 
-    // Listen for messages from worker
-    worker.addEventListener("message", (event) => {
-        switch (event.data.type) {
-            // If there is an error with the web worker
-            case "error":
-                // TODO: better communication with the user
-                tryCount++;
-                worker?.terminate();
-                if (tryCount < MAX_TRY_COUNT) {
+    computeButton?.addEventListener("click", () => {
+        computeButton.remove();
+        // Create web worker. This way is not ideal, but allows for a simpler build process.
+        worker = new Worker("/static/js/client/worker.js");
+
+        // Listen for messages from worker
+        worker.addEventListener("message", (event) => {
+            switch (event.data.type) {
+                // If there is an error with the web worker
+                case "error":
+                    // TODO: better communication with the user
+                    tryCount++;
+                    worker?.terminate();
+                    if (tryCount < MAX_TRY_COUNT) {
+                        forceQuiet = true;
+                        run();
+                    } else {
+                        // TODO set footer with ref for how to solve problem
+                        forceQuiet = false;
+                        tryAlert("Something went wrong in the webworker");
+                    }
+                    break;
+
+                // Web worker telling it's done with its current work
+                case "workDone":
+                    forceQuiet = false;
+                    tryAlert("Web worker task done! Starting a new one.");
+                    tryCount = 0;
+                    worker?.terminate();
+
+                    // Start new worker, but this time quietly
                     forceQuiet = true;
                     run();
-                } else {
-                    // TODO set footer with ref for how to solve problem
-                    forceQuiet = false;
-                    tryAlert("Something went wrong in the webworker");
-                }
-                break;
-
-            // Web worker telling it's done with its current work
-            case "workDone":
-                forceQuiet = false;
-                tryAlert("Web worker task done! Starting a new one.");
-                tryCount = 0;
-                worker?.terminate();
-
-                // Start new worker, but this time quietly
-                forceQuiet = true;
-                run();
-                break;
-        }
+                    break;
+            }
+        });
     });
 };
 
