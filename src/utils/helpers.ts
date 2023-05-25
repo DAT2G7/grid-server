6ece4836581f94232e1055cd3bd057af97b89427
HPT-I <isak@hauptmann.tech>
2023-05-25 12:24:26 +0200

    more comments!

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,11 @@
 import fs from "fs";
 import { SSL_CERT_PATH, SSL_KEY_PATH } from "../config";
 
-export function getSSLCredentials() {
+/**
+ * Responsible for retrieving the SSL credentials from the file system.
+ * @returns An object containing the key and cert, or undefined if the files do not exist.
+ */
+export function getSSLCredentials(): { key: Buffer; cert: Buffer } | undefined {
     if (fs.existsSync(SSL_KEY_PATH) && fs.existsSync(SSL_CERT_PATH)) {
         return {
             key: fs.readFileSync(SSL_KEY_PATH),
@@ -10,7 +14,12 @@ export function getSSLCredentials() {
     }
     return undefined;
 }
-// A non strict check for null also checks for undefined.
+
+/**
+ * Checks if a value is defined, i.e. not null or undefined.
+ * @param value The value to check.
+ * @returns {boolean} true if the value is defined, false if not.
+ */
 export function isDefined<T>(value: T | undefined | null): value is T {
     return value != null;
 }
